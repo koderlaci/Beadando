9355bf55e30656aef20d2e3eb181b3924d9db97d
Kóder László <koder.laci67@gmail.com>
2020-05-09 14:59:30 +0200

    07_Commit

---
+++ templ.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HALJATEK</title>
+	<meta http-equiv="Content-Type" content="text/html"; charset=UTF-8">
+</head>
+<body>
+	<nav id="navigation">
+		<ul id="nav">
+			<li><a href="index.php">Főoldal</a></li>
+			<li><a href="#">Hírek</a></li>
+			<li><a href="#">Játék</a></li>
+			<li><a href="#">Regisztráció</a></li>
+</body>
+</html>
