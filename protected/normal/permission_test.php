2f479805fd8943d3e111408421017de6f23c1807
Kóder László <koder.laci67@gmail.com>
2020-05-12 21:00:38 +0200

    21_Commit

--- protected/normal/permission_test.php
+++ protected/normal/permission_test.php
@@ -1,6 +1,5 @@
 <?php if(!isset($_SESSION['permission']) || $_SESSION['permission'] < 1) : ?>
 	<h1>Hozzáférés megtagadva!</h1>
-	Permission check: <?=isset($_SESSION['permission']) ? $_SESSION['permission'] : "You don't have a permission level." ?>
 <?php else : ?>
 	<h1>Hozzáférés engedélyezve!</h1>
 	<p>A jogosultsági szinted <?=$_SESSION['permission'] ?></p>
